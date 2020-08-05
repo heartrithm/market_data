7dc13b5e52cf3de84a23f3fd4de1b1900ff86451
Charlie Schluting <charlie@schluting.com>
2020-08-05 15:35:15 -0500

    fix package/egg naming in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-
 from setuptools import setup, find_packages
 
 setup(
-    name="candles-sync",
-    version="0.0.1",
-    description="Library and CLI for syncing crypto exchange candles to influxdb",
+    name="market_data",
+    version="0.0.2",
+    description="Library and CLI for syncing crypto exchange data (candles, etc) to influxdb",
     packages=find_packages(),
-    install_requires=["requests"],
 )
