4de2e1c512eaf67dc490a4047d773a93d5e0c1c6
pablomitchell <pablo.mitchell@gmail.com>
2022-03-23 14:41:21 -0700

    added time element to request result set

--- futures/sync_futures.py
+++ futures/sync_futures.py
@@ -1,5 +1,6 @@
 import sys
 
+import arrow
 from ratelimit import limits, sleep_and_retry
 
 from candles.sync_candles import BaseSyncCandles
@@ -11,14 +12,14 @@ IS_PYTEST = "pytest" in sys.modules
 
 
 def get_sync_futures_class(exchange, symbol, interval=None, start=None, end=None, host=None):
-    """ Abstraction layer that returns an instance of the right SyncRates class """
+    """ Abstraction layer that returns an instance of the right SyncCandles class """
     if exchange.lower() == "ftx":
         # FTX only supports 1h
         return SyncFTXFutures(symbol, "1h", start, end, host, data_type="futures")
 
 
 class SyncFTXFutures(BaseSyncCandles):
-    """ Sync funding data for FTX
+    """ Sync futures data for FTX
         Some comes from FTX, some from tardis.dev (historical data).
     """
 
@@ -38,8 +39,8 @@ class SyncFTXFutures(BaseSyncCandles):
     def call_api(self, endpoint, params):
         # FTX futures calls return 1 item
         res = self.client.brequest(1, endpoint=endpoint, params=params)
-        if "nextFundingTime" in res:
-            res["time"] = res["nextFundingTime"]  # helps us play nice with sync
+        if "nextFundingTime" in res["result"]:
+            res["result"]["time"] = arrow.get(res["result"]["nextFundingTime"]).timestamp()
         return res  # one item returned from FTX /futures/ API - add list because we iterate later
 
     def pull_data(self):
