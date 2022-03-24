a0d45cc610338558c13aa56adcd8b82771b54951
pablomitchell <pablo.mitchell@gmail.com>
2022-03-23 18:40:30 -0700

    added funding rates back in (thanks charlie)

--- sync.py
+++ sync.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from candles.sync_candles import get_sync_candles_class
+from funding_rates.sync_rates import get_sync_rates_class
 from futures.sync_futures import get_sync_futures_class
 from loguru import logger
 import click
 import os
 
-COMMANDS = ["candles", "futures"]
+COMMANDS = ["candles", "funding_rates", "futures"]
 logger.level = os.getenv("LOG_LEVEL", "WARNING")
 
 
@@ -22,6 +23,17 @@ def run(*args, **options):  # pragma: no cover
     if options["command"] == "candles":
         exchange = options["exchange"].lower()
         client = get_sync_candles_class(
+            exchange=exchange,
+            symbol=options["symbol"],
+            interval=options["interval"],
+            start=options["start"],
+            end=options["end"],
+        )
+        client.pull_data()
+
+    elif options["command"] == "funding_rates":
+        exchange = options["exchange"].lower()
+        client = get_sync_rates_class(
             exchange=exchange,
             symbol=options["symbol"],
             interval=options["interval"],
