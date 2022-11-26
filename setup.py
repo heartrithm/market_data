c8222c5741c244552c9c632593fa525b01aba45c
Nick Sullivan <nick@sullivanflock.com>
2022-11-26 12:00:35 -0800

    Bump version for ftx changes

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="market_data",
-    version="0.1.6",
+    version="0.2",
     description="Library and CLI for syncing crypto exchange data (candles, etc) to influxdb",
     packages=find_packages(),
-    install_requires=["arrow", "boto3", "influxdb", "loguru", "ratelimit", "requests", "tardis-client", "tardis-dev"],
+    install_requires=[
+        "arrow",
+        "boto3",
+        "influxdb",
+        "loguru",
+        "ratelimit",
+        "requests",
+        "tardis-client",
+        "tardis-dev",
+    ],
 )
